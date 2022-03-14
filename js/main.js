80ed0c389abfc0b22dce7a38c865d40b17d426f3
Chris Small <chris_small08_24@yahoo.com>
2022-03-14 11:06:58 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,187 @@
+
+document.getElementById('subZero').onclick = chooseSubZero
+document.getElementById('scorpion').onclick = chooseScorpion
+document.getElementById('mileena').onclick = chooseMilenna
+document.getElementById('shaoKahn').onclick = chooseShaoKahn
+document.getElementById('frost').onclick = chooseFrost
+document.getElementById('reptile').onclick = chooseReptile
+document.getElementById('cassieCage').onclick = chooseCassieCage
+document.getElementById('liuKang').onclick = chooseLiuKang
+document.getElementById('raiden').onclick = chooseRaiden
+document.getElementById('kitana').onclick = chooseKitana
+document.getElementById('noobSaibot').onclick = chooseNoobSaibot
+document.getElementById('sonyaBlade').onclick = chooseSonyaBlade
+document.getElementById('johnnyCage').onclick = chooseJohnnyCage
+document.getElementById('jade').onclick = chooseJade
+document.getElementById('sheeva').onclick = chooseSheeva
+
+function background() {
+  document.querySelector('body').style.backgroundPosition = 'center'
+  document.querySelector('body').style.backgroundSize = 'cover'
+  document.querySelector('body').style.color = 'red'
+}
+
+function playSubZero() {
+  let audio = document.getElementById('subZeroAudio');
+  audio.play() 
+}
+
+function chooseSubZero() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/subZeroWallpaper.jpg')"
+  background()
+  playSubZero()
+}
+
+function playScorpion() {
+  let audio = document.getElementById('scorpionAudio');
+  audio.play() 
+}
+
+function chooseScorpion() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/scorpionWallpaper.jpg')"
+  background()
+  playScorpion()
+}
+
+function playMileena() {
+  let audio = document.getElementById('mileenaAudio');
+  audio.play() 
+}
+
+function chooseMilenna() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/mileenaWallpaper.jpg')"
+  background()
+  playMileena()
+}
+
+function playShaoKahn() {
+  let audio = document.getElementById('shaoKahnAudio');
+  audio.play() 
+}
+
+function chooseShaoKahn() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/shaoKahnWallpaper.jpg')"
+  background()
+  playShaoKahn()
+}
+
+function playFrost() {
+  let audio = document.getElementById('frostAudio');
+  audio.play() 
+}
+
+function chooseFrost() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/frostWallpaper.jpg')"
+  background()
+  playFrost()
+}
+
+function playReptile() {
+  let audio = document.getElementById('reptileAudio');
+  audio.play() 
+}
+
+function chooseReptile() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/reptileWallpaper.jpg')"
+  background()
+  playReptile()
+}
+
+function playCassieCage() {
+  let audio = document.getElementById('cassieCageAudio');
+  audio.play() 
+}
+
+function chooseCassieCage() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/cassieCageWallpaper.jpg')"
+  background()
+  playCassieCage()
+}
+
+function playLiuKang() {
+  let audio = document.getElementById('liuKangAudio');
+  audio.play() 
+}
+
+function chooseLiuKang() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/liuKangWallpaper.jpg')"
+  background()
+  playLiuKang()
+}
+
+function playRaiden() {
+  let audio = document.getElementById('raidenAudio');
+  audio.play() 
+}
+
+function chooseRaiden() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/raidenWallpaper.jpg')"
+  background()
+  playRaiden()
+}
+
+function playKitana() {
+  let audio = document.getElementById('kitanaAudio');
+  audio.play() 
+}
+
+function chooseKitana() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/kitanaWallpaper.jpg')"
+  background()
+  playKitana()
+}
+ 
+function playNoobSaibot() {
+  let audio = document.getElementById('noobSaibotAudio');
+  audio.play() 
+}
+
+function chooseNoobSaibot() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/noobSaibotWallpaper.jpg')"
+  background()
+  playNoobSaibot()
+}
+
+function playSonyaBlade() {
+  let audio = document.getElementById('sonyaBladeAudio');
+  audio.play() 
+}
+
+function chooseSonyaBlade() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/sonyaBladeWallpaper.jpg')"
+  background()
+  playSonyaBlade()
+}
+
+function playJohnnyCage() {
+  let audio = document.getElementById('johnnyCageAudio');
+  audio.play() 
+}
+
+function chooseJohnnyCage() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/johnnyCageWallpaper.jpg')"
+  background()
+  playJohnnyCage()
+}
+
+function playJade() {
+  let audio = document.getElementById('jadeAudio');
+  audio.play() 
+}
+
+function chooseJade() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/jadeWallpaper.jpg')"
+  background()
+  playJade()
+}
+
+function playSheeva() {
+  let audio = document.getElementById('sheevaAudio');
+  audio.play() 
+}
+
+function chooseSheeva() {
+  document.querySelector('body').style.background = "url('C:/Users/Chris/Downloads/class12-materials/sheevaWallpaper.jpg')"
+  background()
+  playSheeva()
+}
